424017596b401ddf91e041fdfb48d22d47d29398
peet <petrounknown@gmail.com>
2025-08-14 13:47:08 +0300

    Updated, Works Well

--- app.py
+++ app.py
@@ -21,41 +21,7 @@ class App(ctk.CTk):
         self.downloadsPath=str(Path.home() / "Downloads")
         self.createUI()
         
-    def repair_file(file_path, repaired_folder_path):
-        BYTES_TO_KEEP = 4
-        BYTES_TO_ADD = b'\x00' * 12
-        REPAIR_SIZE = 334
-
-        with open(file_path, 'rb') as original_file:
-            # Read the first 4 bytes
-            first_bytes = original_file.read(BYTES_TO_KEEP)
-            
-            # Read the rest of the file content
-            file_content = original_file.read()
-
-            # Remove the last 334 bytes
-            repaired_content = file_content[:-REPAIR_SIZE]
-
-            # Construct repaired content by concatenating first bytes, additional bytes, and truncated content
-            repaired_content = first_bytes + BYTES_TO_ADD + repaired_content
-
-            # Get the file name without extension
-            file_name, file_ext = os.path.splitext(os.path.basename(file_path))
-
-            # Check if the extension is not .mp3, keep iterating to find the correct extension
-            while file_ext.lower() != ".mp3":
-                # If there's no extension left, break the loop
-                if not file_ext:
-                    break
-                # Remove the current extension and check the next one
-                file_name, file_ext = os.path.splitext(file_name)
-
-            # Write the repaired content to the new file in the specified folder
-            repaired_file_path = os.path.join(repaired_folder_path, file_name + ".mp3")
-            with open(repaired_file_path, 'wb') as repaired_file:
-                repaired_file.write(repaired_content)
-            
-            print("Processed:", file_path)
+    
         
 
     def createUI(self):
@@ -77,13 +43,31 @@ class App(ctk.CTk):
         self.console.see("end")
 
     def download_audio(self):
+        if not os.path.exists(str(Path.home() / "Downloads/ytDownloads")):
+            print("Folder ytDownloads does not exist.")
+            self.log_to_console("Folder ytDownloads does not exist.")
+            self.log_to_console("Creating the folder user/Downloads/ytDownloads")
+            os.makedirs(os.path.join(self.downloadsPath, "ytDownloads"))
+            self.repairsPath=str(Path.home() / "Downloads/ytDownloads")
+        else:
+            self.repairsPath=str(Path.home() / "Downloads/ytDownloads")
+            
         link=self.link_entry.get().strip()
         links=link.split(" ")
         if not links:
             self.log_to_console("ERROR: Please enter a YouTube link.")
             return
         self.start_time=0
-        self.start_download(links)
+
+        x=1
+        self.start_time=time.time()
+        for link in links:
+            self.log_to_console(f"STATUS: Starting download {x} of {len(links)}.")
+            time.sleep(1.0)
+            self.start_download(link)
+            x+=1
+
+
         #execution should wait for download completion here!!
         elapsedTime=int(time.time()-self.start_time)
         
@@ -104,75 +88,57 @@ class App(ctk.CTk):
 
         # threading.Thread(target=self.start_download, args=(links,)).start()
 
-    def fixHeader(self):
-        pass
+    
 
-    def start_download(self, links):
-        x=1
-        self.start_time=time.time()
-        print(links)
-        for link in links:
-            self.log_to_console(f"STATUS: Starting download {x} of {len(links)}.")
-            try:
-                # self.log_to_console("Initializing download...")
-                downloadsPath=str(Path.home() / "Downloads")
-                # command=[
-                #     "yt-dlp", "-x", "--audio-format", "mp3",
-                #     "--ffmpeg-location", "c:/Users/<username>/ffmpeg-full_build/bin",
-                #     "-o", f"{downloadsPath}/%(title)s.%(ext)s", link,
-                # ]
+    def start_download(self, link):
+        try:
+            yt = YouTube(url=f"{link}")
+            title=yt.title
+            length=yt.length
+            
+            lengthdy=int(length/86400)
+            lengthhr=int(int(length-int(lengthdy*86400))/3600)
+            lengthmin=int(int(length-int(lengthdy*86400)-int(lengthhr*3600))/60)
+            lengthsec=int(length-int(lengthdy*86400)-int(lengthhr*3600)-int(lengthmin*60)) #Works:|
 
-                yt = YouTube(url=f"{link}")
-                # print(yt)
-                title=yt.title
-                length=yt.length
+            # https://www.youtube.com/watch?v=y5DAgBaS53Y
 
-                lengthdy=int(length/86400)
-                lengthhr=int(int(length-int(lengthdy*86400))/3600)
-                lengthmin=int(int(length-int(lengthdy*86400)-int(lengthhr*3600))/60)
-                lengthsec=int(length-int(lengthdy*86400)-int(lengthhr*3600)-int(lengthmin*60)) #Works:|
+            if lengthsec <= 9:
+                lengthsec=f"0{lengthsec}"
+            elif lengthsec > 9:
+                lengthsec=f"{lengthsec}"
+            else:
+                lengthsec=f"00" #Works:|
+            
+            self.log_to_console(f"\tTitle: {title}")
 
-                if lengthsec <= 9:
-                    lengthsec=f"0{lengthsec}"
-                elif lengthsec > 9:
-                    lengthsec=f"{lengthsec}"
-                else:
-                    lengthsec=f"00" #Works:|
+            if lengthdy == 0:
+                self.log_to_console(f"\tLength: {lengthhr}:{lengthmin}:{lengthsec}.")
+            else:
+                self.log_to_console(f"\tLength: {lengthdy}Days {lengthhr}Hours {lengthmin}Minutes {lengthsec}Seconds.")
 
-                self.log_to_console(f"\tTitle: {title}")
+            
+            
+            
+            ourAudioList=yt.streams.filter(only_audio=True).first()
+            self.fileName=f"{title}.webm"
+            ourAudioList.download(output_path=self.downloadsPath,filename=self.fileName)
 
-                if lengthdy == 0:
-                    self.log_to_console(f"\tLength: {lengthhr}:{lengthmin}:{lengthsec}.")
-                else:
-                    self.log_to_console(f"\tLength: {lengthdy}Days {lengthhr}Hours {lengthmin}Minutes {lengthsec}Seconds.")
+            input_file=os.path.join(self.downloadsPath,self.fileName)
+            output_file=os.path.join(self.repairsPath,f"{title}.mp3")
 
-                
-                
+            ffmpeg.input(input_file).output(output_file,acodec="libmp3lame",audio_bitrate="320k",write_id3v1="1",id3v2_version="3").run(quiet=True)
 
-                ourAudioList=yt.streams.filter(only_audio=True).first()
-                ourAudioList.download(output_path=f"{downloadsPath}",filename=f"{title}.webm")
-                # ourAudioList[0].download(output_path=f"{downloadsPath}",filename=f"{title}")
+            if os.path.exists(input_file):
+                os.remove(input_file)
+                print(f"{input_file} has been deleted.")
+            else:
+                print(f"{input_file} does not exist.")
 
-                # process=subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
-                
 
-                # for line in process.stdout:
-                #     if "downloaded" in line.lower():
-                #         self.log_to_console(line.strip())
-                #     elif "speed" in line.lower():
-                #         self.log_to_console(line.strip())
-
-                # process.wait()
-                
-
-                # if process.returncode==0:
-                #     self.log_to_console(f"COMPLETED! Total time: {elapsed_time:.2f} seconds.")
-                # else:
-                #     self.log_to_console("Error: Download failed.")
-
-            except Exception as e:
-                self.log_to_console(f"Exception: {e}\n")
-            x+=1
+        except Exception as e:
+            self.log_to_console(f"Exception: {e}\n")
+            
 
 if __name__=="__main__":
     app=App()
